d716f7c24286847c2dd03fa98cac3d89c1e521fa
Brittany Barden <bbarden93@yahoo.com>
2018-03-14 17:06:49 -0700

    basic setup

---
+++ index.js
@@ -0,0 +1,23 @@
+const  
+    dotenv = require('dotenv').config(),
+    express = require('express'),
+    app = express(),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    axios = require('axios'),
+    httpClient = axios.create(),
+    PORT = 3000
+    
+const apiKey = process.env.apiKey
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(express.static(`${__dirname}/public`))
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+})
+
+app.listen(PORT, (err)=> {
+    console.log(err || `Server running on ${PORT}`)
+})
